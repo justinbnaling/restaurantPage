0ea1ac6565abdf30ada741cf4ef932a5f524930d
Justin Naling <justinbnaling@gmail.com>
2023-04-29 09:15:12 -0500

    added form to contact page

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -1,12 +1,61 @@
+import './contact.css'
+
 const contact = () => {
     const container = document.createElement('div');
-    container.classList.add("contact")
+    const heading = document.createElement('h1');
+    heading.textContent = "Contact Us";
 
-    const article = document.createElement("div")
-    article.classList.add("articleContainer");
-    article.textContent = "this is the contact page "
+    const formContainer = document.createElement('div');
+    formContainer.classList.add("formContainer")
 
-    container.appendChild(article)
+    const contactForm = document.createElement('form');
+    contactForm.method = "post"
+
+    const labelFullName = document.createElement('label');
+    labelFullName.textContent = "Full Name";
+    labelFullName.htmlFor = "fullName";
+
+    const inputFullName = document.createElement('input');
+    inputFullName.type = "text";
+    inputFullName.name = "fullName";
+    inputFullName.id = "fullName"
+    inputFullName.required = ""
+
+    const labelEmail = document.createElement('label');
+    labelEmail.textContent = "Email";
+    labelEmail.htmlFor = "email";
+
+    const inputEmail = document.createElement('input');
+    inputEmail.type = "email";
+    inputEmail.name = "email";
+    inputEmail.id = "email"
+    inputEmail.required = ""
+
+    const labelMessage = document.createElement('label');
+    labelMessage.textContent = "Message";
+    labelMessage.htmlFor = "message";
+
+    const inputMessage = document.createElement('textArea');
+    inputMessage.name = "message";
+    inputMessage.id = "email"
+    inputMessage.required = ""
+    inputMessage.cols = "20"
+    inputMessage.rows = "10"
+
+    
+    contactForm.appendChild(labelFullName)
+    contactForm.appendChild(inputFullName)
+    contactForm.appendChild(labelEmail)
+    contactForm.appendChild(inputEmail)
+    contactForm.appendChild(labelMessage)
+    contactForm.appendChild(inputMessage)
+
+    formContainer.appendChild(contactForm)
+
+
+    container.appendChild(heading)
+    container.appendChild(formContainer)
+    container.appendChild(formContainer)
 
     return container
 }
